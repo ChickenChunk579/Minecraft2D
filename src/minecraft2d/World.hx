1be407abd9b1bbb2f9664e5c7d81e86bb5ee105e
Rhys Tumelty <rhys@tumelty.co.uk>
2024-12-14 14:25:50 +0000

    Added icon and rough scaffold

---
+++ src/minecraft2d/World.hx
@@ -0,0 +1,13 @@
+package minecraft2d;
+import openfl.display.Tilemap;
+import openfl.display.Sprite;
+
+class World extends Sprite {
+    public var tilemap: Tilemap;
+    
+    public function new() {
+        super();
+
+        //tilemap = new Tilemap(1024, 256, );
+    }
+}
